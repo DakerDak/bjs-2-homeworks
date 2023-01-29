a3fc8c820c746d7af746e8866d7b425b5881db6f
Trevor Quinn <dilam-rt@yandex.ru>
2023-01-29 21:11:44 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant < 0) {
+   arr = [];
+  }
+
+  if (discriminant === 0) {
+    arr = -b/(2*a);
+    arr = [arr];
+  }
+
+  if (discriminant > 0) {
+    let r1 = (-b + Math.sqrt(discriminant) )/(2*a);
+    let r2 = (-b - Math.sqrt(discriminant) )/(2*a);
+    arr = [r1, r2];
+  }
   
   return arr;
 }
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
